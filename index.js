9e9db4e9c2d2bb25c26f5b1aa954d07c4e202911
Paulo Victor <49046915+PauloVictorLira@users.noreply.github.com>
2023-01-19 14:00:48 +0000

    Update Get IP

--- index.js
+++ index.js
@@ -1,12 +1,61 @@
 console.log(`Hello Node.js v${process.versions.node}!`);
 
+// IP FETCH
+
+// const fetch = require('node-fetch');
+
+// (async function main () {
+//   const response = await fetch('https://api.ipify.org/?format=json');
+//   const data = await response.json();
+// console.log(data.ip);
+// })();
+
+
+
+
+
+////IP
+
+async function myIP() {
+  let axios = require('axios');
+  // let cheerio = require('cheerio');
+  let response = await axios.get('https://api.ipify.org/?format=json');
+  let data = response.data.ip;
+  let result = data;
+  console.log(result);
+}
+
+console.log(myIP());
+
+// //DISTÂNCIA
+
+// async function Rodar() {
+//   let axios = require('axios');
+//   // let cheerio = require('cheerio');
+//   let response = await axios.get('https://br.distanciacidades.net/'); //https://br.distanciacidades.net/calcular?from=santos&to=cubatao
+//   let data = response.data.data;
+//   let result = data[0];
+//   console.log(result);
+// }
+
+// console.log(Rodar());
+
+
+
+//DISTÂNCIA
+
 async function Rodar() {
   let axios = require('axios');
   // let cheerio = require('cheerio');
-  let response = await axios.get('https://br.distanciacidades.net/'); //https://br.distanciacidades.net/calcular?from=santos&to=cubatao
-  let data = response.data.data;
+  let response = await axios.get('https://br.distanciacidades.net/calcular?from=santos&to=cubatao');
+  let data = response.data.data.data;
   let result = data[0];
   console.log(result);
 }
 
 console.log(Rodar());
+
+
+
+
+
